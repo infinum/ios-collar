f892dfe636baebbf86620445546c183acb399df1
petar-jadek <petar.jadek@infinum.com>
2025-01-21 08:31:23 +0100

    Add code of conduct, contributing guide and update security policy

--- Example/Collar/ViewController.swift
+++ Example/Collar/ViewController.swift
@@ -16,7 +16,9 @@ class ViewController: UIViewController {
         
         LogItemPopupQueue.shared.enabled = true
         LogItemPopupQueue.shared.showOnView = { UIApplication.shared.keyWindow }
-        
+
+        AnalyticsCollectionManager.shared.printSth()
+
         DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
             AnalyticsCollectionManager.shared.log(event: "Test Event", parameters: [
                 "param1": "value1",
